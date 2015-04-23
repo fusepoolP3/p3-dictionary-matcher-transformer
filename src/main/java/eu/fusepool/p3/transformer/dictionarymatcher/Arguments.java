92275d2aed5438d88631e1dfe187b7d725d17589
Gabor Remenyi <gabor.remenyi@fusepool.net>
2015-04-23 08:54:11 +0200

    default port changed to 8301

--- src/main/java/eu/fusepool/p3/transformer/dictionarymatcher/Arguments.java
+++ src/main/java/eu/fusepool/p3/transformer/dictionarymatcher/Arguments.java
@@ -6,7 +6,7 @@ import org.wymiwyg.commons.util.arguments.CommandLine;
 public interface Arguments extends ArgumentsWithHelp {
 
     @CommandLine(longName = "port", shortName = {"P"}, required = false,
-            defaultValue = "7100",
+            defaultValue = "8301",
             description = "The port on which the proxy shall listen")
     public int getPort();
 
